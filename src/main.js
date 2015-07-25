45df211752e01d9234f383f98189fa028883fe2a
elias94 <elia.scotto@vallauri.edu>
2015-07-24 20:07:29 -0700

    Added Treemap datachart

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 var React = require('react');
+var _ = require('underscore');
 var DataGrid = require('react-datagrid');
 var data = require('../data/sysdig.json');
+
 import HelloWorld from './test';
 import Table from './table.jsx';
+import Chart from './chart.jsx';
 
-// var data = [
-//     {id: 1, title: 'test'},
-//     {id: 2, title: 'test2'}
-// ];
+var Treemap = Chart.Treemap;
+var BarChart = Chart.BarChart;
 
 var columns = [
     {name: 'evt.cpu'},
@@ -20,6 +21,19 @@ var columns = [
     {name: 'proc.name'},
     {name: 'proc.pid'},
     {name: 'thread.pid'}
+];
+
+function createDataObj(event) {
+    return { label: event[0], value: event[1] };
+}
+
+console.log(data.events[0]);
+var parsedData = _.chain(data.events).countBy('proc.name').pairs().map(createDataObj).value();
+
+var treemapData = [
+  {label: "China", value: 1364},
+  {label: "India", value: 1296},
+  {label: "Brazil", value: 203}
 ];
 
 
@@ -38,20 +52,38 @@ var SysdigTable = React.createClass({
     }
 });
 
+var SysdigTreemap = React.createClass({
+    render: function() {
+        return (
+            <Treemap
+              data={parsedData}
+              width={850}
+              height={650}
+              textColor="#484848"
+              fontSize="10px"
+              title="Process name"/>
+        );
+    }
+});
+
 
 React.render(
      <SysdigTable/>,
     document.getElementById('content-table')
 );
 
-React.render(<Table
-             results={data.events}
-             showFilter={true}
-             showSettings={true}
-             enableInfiniteScroll={true}
-             useFixedHeader={true}
-             bodyHeight={400}/>, document.getElementById('griddle-table'));
-// React.render(
-//         <HelloWorld/>,
-//     document.getElementById('hello-world')
-// );
+React.render(
+    <SysdigTreemap/>,
+    document.getElementById('treemap-chart')
+);
+
+React.render(
+    <Table
+        results={data.events}
+        showFilter={true}
+        showSettings={true}
+        enableInfiniteScroll={true}
+        useFixedHeader={true}
+        bodyHeight={400}/>,
+    document.getElementById('griddle-table')
+);
